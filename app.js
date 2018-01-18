31065ebd69fcc3c1dc752417bd6c8afb3ba2c3e6
ryhord <ryhord@users.noreply.github.com>
2018-01-17 22:29:12 -0500

    converted variable values to arrays. map() the working max onto array. created non-working test for findWorkingMax funcion

--- app.js
+++ app.js
@@ -5,27 +5,23 @@ function findWorkingMax(oneRepMax) {
 }
 
 function calcweight1(workingMax) {
-	var set1 = workingMax * 0.65;
-	var set2 = workingMax * 0.75;
-	var set3 = workingMax * 0.85;
+	var week1weights = [0.65, 0.75, 0.85];
+	return week2weights.map( x => x * workingMax);
 }
 
 function calcweight2(workingMax) {
-	var set1 = workingMax * 0.70;
-	var set2 = workingMax * 0.80;
-	var set3 = workingMax * 0.90;
+	var week2weights = [0.70, 0.80, 0.90];
+	return week1weights.map( x => x * workingMax);
 }
 
 function calcweight3(workingMax) {
-	var set1 = workingMax * 0.75;
-	var set2 = workingMax * 0.85;
-	var set3 = workingMax * 0.95;
+	var week3weights = [0.75, 0.85, 0.95];
+	return week1weights.map( x => x * workingMax);
 }
 
 function calcweight4(workingMax) {
-	var set1 = workingMax * 0.40;
-	var set2 = workingMax * 0.50;
-	var set3 = workingMax * 0.60;
+	var week4weights = [0.40, 0.50, 0.60];
+	return week1weights.map( x => x * workingMax);
 }
 
-//vars should be reduced to arrays; .map() to apply working max to them;
+export findWorkingMax
