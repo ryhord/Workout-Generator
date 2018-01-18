b5f2df081e7e1e87776bbe95cbbddb3e179bc4e6
ryhord <ryhord@users.noreply.github.com>
2018-01-18 08:59:53 -0500

    fixed typo on variable names

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ function findWorkingMax(oneRepMax) {
 
 function calcweight1(workingMax) {
 	var week1weights = [0.65, 0.75, 0.85];
-	return week2weights.map( x => x * workingMax);
+	return week1weights.map( x => x * workingMax);
 }
 
 function calcweight2(workingMax) {
 	var week2weights = [0.70, 0.80, 0.90];
-	return week1weights.map( x => x * workingMax);
+	return week2weights.map( x => x * workingMax);
 }
 
 function calcweight3(workingMax) {
 	var week3weights = [0.75, 0.85, 0.95];
-	return week1weights.map( x => x * workingMax);
+	return week3weights.map( x => x * workingMax);
 }
 
 function calcweight4(workingMax) {
 	var week4weights = [0.40, 0.50, 0.60];
-	return week1weights.map( x => x * workingMax);
+	return week4weights.map( x => x * workingMax);
 }
 
 export findWorkingMax
