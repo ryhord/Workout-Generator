951cad8018fee41f06bf8ff63b2d673515470994
ryhord <ryhord@users.noreply.github.com>
2018-01-22 22:00:29 -0500

    grabs user input and displays it on the next page

--- routes/workingmax.js
+++ routes/workingmax.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GEt/post the working max */
-router.get('/', function(req, res) {
-	res.render('workingmax');
+router.post('/', function(req, res) {
+	var weight = {
+		oneRepMax : req.body.oneRepMax,
+	}
+	console.log(weight);
+	res.render('workingmax', { workingmax: weight.oneRepMax });
 });
 
 module.exports = router;
