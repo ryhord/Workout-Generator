12ac723659b42235b139934dc3eff5b2b896094e
ryhord <ryhord@users.noreply.github.com>
2018-01-23 17:18:08 -0500

    added logic in routes to calculate and display working max

--- routes/workingmax.js
+++ routes/workingmax.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var calc = require('../calculations');
 
 /* GEt/post the working max */
 router.post('/', function(req, res) {
 	var weight = {
 		oneRepMax : req.body.oneRepMax,
 	}
+
+	var workingMax = calc.findWorkingMax(weight.oneRepMax);
+
 	console.log(weight);
-	res.render('workingmax', { workingmax: weight.oneRepMax });
+	res.render('workingmax', { workingmax: workingMax });
 });
 
 module.exports = router;
