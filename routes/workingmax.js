97e5ef1ce17b3061f6f71c7cafaa0d0701fb1d53
ryhord <ryhord@users.noreply.github.com>
2018-01-24 23:17:09 -0500

    added a view for results. stored workingMax in cookie, accessed in another route to produce desired results.

--- routes/workingmax.js
+++ routes/workingmax.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 var calc = require('../calculations');
 
+
 /* GEt/post the working max */
 router.post('/', function(req, res) {
 	var weight = {
-		oneRepMax : req.body.oneRepMax,
+		oneRepMax: req.body.oneRepMax,
 	}
 
 	var workingMax = calc.findWorkingMax(weight.oneRepMax);
 
-	console.log(weight);
+	console.log(workingMax);
+	res.cookie('working_max', workingMax);
 	res.render('workingmax', { workingmax: workingMax });
+});
+
+router.get('/results', function(req, res) {
+	var week1weights = calc.calcweight1(req.cookies.working_max);
+	res.render('results', { results: week1weights });
 });
 
 module.exports = router;
