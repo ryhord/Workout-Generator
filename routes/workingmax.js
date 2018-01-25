b950aa64a8c78e99d80c8f2845e63c00e0434e92
ryhord <ryhord@users.noreply.github.com>
2018-01-25 15:29:52 -0500

    added logic to calculate the 4 working max numbers at once and display them in view

--- routes/workingmax.js
+++ routes/workingmax.js
@@ -6,14 +6,25 @@ var calc = require('../calculations');
 /* GEt/post the working max */
 router.post('/', function(req, res) {
 	var weight = {
-		oneRepMax: req.body.oneRepMax,
+		pressMax: req.body.pressMax,
+		deadMax: req.body.deadMax,
+		benchMax: req.body.benchMax,
+		squatMax: req.body.squatMax,
 	}
 
-	var workingMax = calc.findWorkingMax(weight.oneRepMax);
-
-	console.log(workingMax);
-	res.cookie('working_max', workingMax);
-	res.render('workingmax', { workingmax: workingMax });
+	var pressWorkingMax = calc.findWorkingMax(weight.pressMax);
+	var deadWorkingMax = calc.findWorkingMax(weight.deadMax);
+	var benchWorkingMax = calc.findWorkingMax(weight.benchMax);
+	var squatWorkingMax = calc.findWorkingMax(weight.squatMax);
+	console.log(pressWorkingMax);
+	console.log(deadWorkingMax);
+	console.log(benchWorkingMax);
+	console.log(squatWorkingMax);
+	res.cookie('pressWorking_Max', pressWorkingMax);
+	res.cookie('deadWorking_Max', deadWorkingMax);
+	res.cookie('benchWorking_Max', benchWorkingMax);
+	res.cookie('squatWorking_Max', squatWorkingMax);
+	res.render('workingmax', { pressWorkingMax: pressWorkingMax, deadWorkingMax: deadWorkingMax, benchWorkingMax: benchWorkingMax, squatWorkingMax: squatWorkingMax });
 });
 
 router.get('/results', function(req, res) {
