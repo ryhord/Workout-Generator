6590f752f22d20bb96ab26ae845421b8e5bd00c1
ryhord <ryhord@users.noreply.github.com>
2018-01-26 15:42:45 -0500

    modified functions to round to the nearest multiple of 5

--- calculations.js
+++ calculations.js
@@ -6,22 +6,38 @@
 
 	function calcweight1(workingMax) {
 		var week1weights = [0.65, 0.75, 0.85];
-		return week1weights.map( x => x * workingMax).join(', ');
+		week1weights = week1weights.map( x => x * workingMax); //mult workingmax by all values
+		week1weights = week1weights.map(function(x){
+			return (x % 5) >= 2.5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5;
+		}); // rounds result to the nearest multiple of 5		
+		return week1weights.join(', ');
 	};
 
 	function calcweight2(workingMax) {
 		var week2weights = [0.70, 0.80, 0.90];
-		return week2weights.map( x => x * workingMax).join(', ');
+		week2weights = week2weights.map( x => x * workingMax); //mult workingmax by all values
+		week2weights = week2weights.map(function(x){
+			return (x % 5) >= 2.5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5;
+		}); // rounds result to the nearest multiple of 5		
+		return week2weights.join(', ');
 	};
 
 	function calcweight3(workingMax) {
 		var week3weights = [0.75, 0.85, 0.95];
-		return week3weights.map( x => x * workingMax).join(', ');
+		week3weights = week3weights.map( x => x * workingMax); //mult workingmax by all values
+		week3weights = week3weights.map(function(x){
+			return (x % 5) >= 2.5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5;
+		}); // rounds result to the nearest multiple of 5		
+		return week3weights.join(', ');
 	};
 
 	function calcweight4(workingMax) {
 		var week4weights = [0.40, 0.50, 0.60];
-		return week4weights.map( x => x * workingMax).join(', ');
+		week4weights = week4weights.map( x => x * workingMax); //mult workingmax by all values
+		week4weights = week4weights.map(function(x){
+			return (x % 5) >= 2.5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5;
+		}); // rounds result to the nearest multiple of 5		
+		return week4weights.join(', ');
 	};
 
 //calcweight functions need to format results to allow only one decimal place
