7937460e2f9f2d9612484e9aa79a7bfe104c148d
ryhord <ryhord@users.noreply.github.com>
2018-01-29 15:27:10 -0500

    modified variables to use app.locals and not cookies

--- routes/workingmax.js
+++ routes/workingmax.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var calc = require('../calculations');
-
 var app = express();
 
 
@@ -20,47 +19,43 @@ router.post('/', function(req, res) {
 		squatMax: app.locals.squatMax,
 	}
 
-	var pressWorkingMax = calc.findWorkingMax(weight.pressMax);
-	var deadWorkingMax = calc.findWorkingMax(weight.deadMax);
-	var benchWorkingMax = calc.findWorkingMax(weight.benchMax);
-	var squatWorkingMax = calc.findWorkingMax(weight.squatMax);
+	app.locals.pressWorkingMax = calc.findWorkingMax(weight.pressMax);
+	app.locals.deadWorkingMax = calc.findWorkingMax(weight.deadMax);
+	app.locals.benchWorkingMax = calc.findWorkingMax(weight.benchMax);
+	app.locals.squatWorkingMax = calc.findWorkingMax(weight.squatMax);
 
-	res.cookie('press_working_max', pressWorkingMax);
-	res.cookie('dead_working_max', deadWorkingMax);
-	res.cookie('bench_working_max', benchWorkingMax);
-	res.cookie('squat_working_max', squatWorkingMax);
 	res.render('workingmax', { 
-		pressWorkingMax: pressWorkingMax, 
-		deadWorkingMax: deadWorkingMax, 
-		benchWorkingMax: benchWorkingMax, 
-		squatWorkingMax: squatWorkingMax 
+		pressWorkingMax: app.locals.pressWorkingMax, 
+		deadWorkingMax: app.locals.deadWorkingMax, 
+		benchWorkingMax: app.locals.benchWorkingMax, 
+		squatWorkingMax: app.locals.squatWorkingMax 
 	});
 });
 
 router.get('/results', function(req, res) {
 	//Week 1 Results (possibly reduced to arrays, calc function mapped)
-	var week1PressWeights = calc.calcweight1(req.cookies.press_working_max);
-	var week1DeadWeights = calc.calcweight1(req.cookies.dead_working_max);
-	var week1BenchWeights = calc.calcweight1(req.cookies.bench_working_max);
-	var week1SquatWeights = calc.calcweight1(req.cookies.squat_working_max);
+	var week1PressWeights = calc.calcweight1(app.locals.pressWorkingMax);
+	var week1DeadWeights = calc.calcweight1(app.locals.deadWorkingMax);
+	var week1BenchWeights = calc.calcweight1(app.locals.benchWorkingMax);
+	var week1SquatWeights = calc.calcweight1(app.locals.squatWorkingMax);
 
 	//Week 2 Results
-	var week2PressWeights = calc.calcweight2(req.cookies.press_working_max);
-	var week2DeadWeights = calc.calcweight2(req.cookies.dead_working_max);
-	var week2BenchWeights = calc.calcweight2(req.cookies.bench_working_max);
-	var week2SquatWeights = calc.calcweight2(req.cookies.squat_working_max);
+	var week2PressWeights = calc.calcweight2(app.locals.pressWorkingMax);
+	var week2DeadWeights = calc.calcweight2(app.locals.deadWorkingMax);
+	var week2BenchWeights = calc.calcweight2(app.locals.benchWorkingMax);
+	var week2SquatWeights = calc.calcweight2(app.locals.squatWorkingMax);
 	
 	//Week 3 Results
-	var week3PressWeights = calc.calcweight3(req.cookies.press_working_max);
-	var week3DeadWeights = calc.calcweight3(req.cookies.dead_working_max);
-	var week3BenchWeights = calc.calcweight3(req.cookies.bench_working_max);
-	var week3SquatWeights = calc.calcweight3(req.cookies.squat_working_max);
+	var week3PressWeights = calc.calcweight3(app.locals.pressWorkingMax);
+	var week3DeadWeights = calc.calcweight3(app.locals.deadWorkingMax);
+	var week3BenchWeights = calc.calcweight3(app.locals.benchWorkingMax);
+	var week3SquatWeights = calc.calcweight3(app.locals.squatWorkingMax);
 	
 	//Week 4 Results
-	var week4PressWeights = calc.calcweight4(req.cookies.press_working_max);
-	var week4DeadWeights = calc.calcweight4(req.cookies.dead_working_max);
-	var week4BenchWeights = calc.calcweight4(req.cookies.bench_working_max);
-	var week4SquatWeights = calc.calcweight4(req.cookies.squat_working_max);
+	var week4PressWeights = calc.calcweight4(app.locals.pressWorkingMax);
+	var week4DeadWeights = calc.calcweight4(app.locals.deadWorkingMax);
+	var week4BenchWeights = calc.calcweight4(app.locals.benchWorkingMax);
+	var week4SquatWeights = calc.calcweight4(app.locals.squatWorkingMax);
 
 	//Template Render (need to be easier way, and to load results dynamically based on number of months wanted)
 	res.render('results', { 
